0fb808f3e5bd5acd076e1e701e6099d0a8468f52
Oskar Nyberg <oskar@mullvad.net>
2020-04-16 10:28:53 +0200

    Fix modal flickering

--- gui/src/shared/scheduler.ts
+++ gui/src/shared/scheduler.ts
@@ -1,9 +1,9 @@
 export class Scheduler {
   private timer?: NodeJS.Timeout;
 
-  public schedule(action: () => void, delay: number) {
+  public schedule(action: () => void, delay?: number) {
     this.cancel();
-    this.timer = global.setTimeout(action, delay);
+    this.timer = global.setTimeout(action, delay ?? 0);
   }
 
   public cancel() {
